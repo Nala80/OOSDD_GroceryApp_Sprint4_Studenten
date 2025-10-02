f5e672fcbe2d3a868471533234f32168c081b73d
Nalawi Woldeyohannes <nalawi.woldeyohannes@hva.nl>
2025-10-02 21:27:24 +0200

    UC13 tot stap 4 gemaakt.

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,19 @@
 ﻿
+using System.Data;
+
 namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
-        public Client(int id, string name, string emailAddress, string password) : base(id, name)
+        public enum Role { None, Admin }
+        public Role ClientRole { get; set; } = Role.None;
+        public Client(int id, string name, string emailAddress, string password, Role clientRole) : base(id, name)
         {
             EmailAddress=emailAddress;
             Password=password;
+            ClientRole = clientRole;
         }
     }
 }
